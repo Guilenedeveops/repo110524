43315820d81294599a098420ee218b77788c94cf
guilene <gpascale.fusillier@gmail.com>
2024-05-11 18:01:37 +0200

    fc

--- backend.tf
+++ backend.tf
@@ -1,12 +1,11 @@
 
-/*
 terraform {
   backend "s3" {
-    bucket         = "week10-sm-terraform"
+    bucket         = "week10-sm-terraform110524"
     key            = "test/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "state-login"
   }
 }
-*/
+
